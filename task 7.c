820d68ddff7fe063487127bc00173aa00112f5c2
LevLevinson <120116943+LevLevinson@users.noreply.github.com>
2023-01-12 12:20:00 +0200

    Update task 7.c

--- task 7.c
+++ task 7.c
@@ -1,11 +1,3 @@
-/*7. קלטו מהמשתמש מחרוזת, כל עוד המחרוזת שהוא הכניס
-קטנה מ 10 תוים המשיכו לבקש ממנו שיכניס עוד מחרוזת
-עד שהיא תהיה בגודל 10 תוים (אפשר להניח שהיוזר לא יכניס יותר מ 100 תוים)
-*/
-
-
-
-
 
 #include <stdio.h>
 #include <string.h>
@@ -17,24 +9,20 @@ int main()
 	printf("Please Enter a String:\n");
 	gets(name);
 	len = strlen(name);
-	while (len <= 10)
+	while (len < 10)
 	{
-		if (len == 10)
-		{
-
-			printf("You did it! Your string is:%s\n", name);
-			name[len] = NULL;
-
-		}
-		else
-		{
 			printf("Please Enter another String:\n");
 			gets(name);
 			len = strlen(name);
-		}
 
 	}
+    if (len == 10)
+		{
 
+			printf("You did it! Your string is: %s ,Your String lengh is %d chars long\n", name,len);
+			name[len] = NULL;
+
+		}
 
 	return 0;
 }
